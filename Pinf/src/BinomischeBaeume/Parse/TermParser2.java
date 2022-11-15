84e867f7f4d313827d82c9cbe2c5d9695a970f97
marco411 <vorndamme2003@gmail.com>
2022-11-15 13:10:46 +0100

    15.11.2022  eigenarbeit

--- Pinf/src/BinomischeBaeume/Parse/TermParser2.java
+++ Pinf/src/BinomischeBaeume/Parse/TermParser2.java
@@ -7,11 +7,13 @@ public class TermParser2 {
     String[] arr;
 
     private final String input;
-    private String output;
-    private  ArrayList<String> stack = new ArrayList<>();
+    private ArrayList<Character> output = new ArrayList<>();
+    private ArrayList<Character> stack = new ArrayList<>();
     public TermParser2(String input){
         this.input = input;
     }
+
+    private boolean test = true;
 
 
     public String[] parse() {
@@ -23,17 +25,29 @@ public class TermParser2 {
 
         for(int i = 0; i< input.length(); i++){
             if( CharacterLists.NUMBERS.contains(temp.charAt(i)) && temp.charAt(i) == '.' ){
-                output += temp.charAt(i);
+                output.add(temp.charAt(i));
             }
 
             if(CharacterLists.OPERATORS.contains(temp.charAt(i))){
-                stack.add(String.valueOf(temp.charAt(i)));
+                stack.add(temp.charAt(i));
             }
 
            if(CharacterLists.CHARACTERS.contains(temp.charAt(i))){
-               while(stack != null && CharacterLists.CHARACTERS.contains(stack.get(0)) && ){
+               do{
+                   test = false;
+                   if(stack.size() > 0 && CharacterLists.CHARACTERS.contains(stack.get(stack.size()-1)) && temp.charAt(i) ) {
+                       output.add(stack.get(stack.size() - 1));
+                       test = true;
+                   }
+               }while(test == true);
+               stack.add(temp.charAt(i));
+           }
 
-               }
+           if(temp.charAt(i) == '('){
+               stack.add(temp.charAt(i));
+           }
+           if(temp.charAt(i) == ')'){
+               if()
            }
         }
 
--- Pinf/src/BinomischeBaeume/Parse/test.java
+++ Pinf/src/BinomischeBaeume/Parse/test.java
@@ -6,6 +6,5 @@ import java.util.ArrayList;
 
 public class test {
 
-    @Test
 
 }
