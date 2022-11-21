8aec23c3cbc4bff1ca0d72f2ba1a28b5124e94c9
marco411 <vorndamme2003@gmail.com>
2022-11-21 11:24:44 +0100

    neues Projekt 21.11.2022

---
+++ Pinf/PolynomBechnung/src/Polynom.java
@@ -0,0 +1,53 @@
+public class Polynom {
+
+
+    private String input;
+    private double[] coefficient ;
+
+
+    public Polynom(double[] coefficient ) {
+        this.coefficient = coefficient;
+    }
+
+
+    public String output(double[] coefficient, double input){
+
+
+
+
+    }
+
+
+
+//following methods are unfinished or not build in the right way atm
+    public void calculateX(){
+
+    }
+
+    public void getHighestPotenz(){
+
+    }
+
+
+
+    public void checkPolynomSymmetrical(String[] coefficient){
+
+    }
+
+
+
+    public void checkPolynomAsymmetrical(String[] coefficient){
+
+    }
+
+
+    public void getFirstDerivation(String[] coefficient){
+
+    }
+
+
+    public void getSecondDerivation(String[] coefficient){
+
+    }
+
+}
