7d5aded86d50795f880579e9b8d2d21456034cf4
marco411 <vorndamme2003@gmail.com>
2022-11-22 13:15:31 +0100

    update 22.11.2022

--- Pinf/PolynomBechnung/src/Polynom.java
+++ Pinf/PolynomBechnung/src/Polynom.java
@@ -1,54 +1,94 @@
 public class Polynom {
 
+    private final double[] coefficients;
+    private String symmetry;
+    private String highestPotenz;
+    public final String[] SYMMETRIES = new String[] {"Axisymmetric", "Pointsymmetric", "No symmetry"};
 
-    private String input;
-    private double[] coefficient ;
-
-
-    public Polynom(double[] coefficient ) {
-        this.coefficient = coefficient;
+    public Polynom(double[] coefficients) {
+        this.coefficients = coefficients;
+        checkForSymmetry();
+        highestPotenz();
     }
 
+    public String getHighestPotenz() {
+        return highestPotenz;
+    }
 
-    public String output(double[] coefficient, double input){
-        String outputString;
-        for(int i = 0; i < coefficient.length; i++){
-            
+    public String getSymmetry() {
+        return symmetry;
+    }
+
+    //following methods are unfinished or not build in the right way atm
+    public double calculateY(double x) {
+        double sum = 0.;
+        for (int i = 0; i < coefficients.length; i++) {
+            sum += coefficients[i] * Math.pow(x, i);
         }
-        return outputString;
+        return sum;
     }
 
 
+    private void highestPotenz() {
+        int potenz = 0;
 
-//following methods are unfinished or not build in the right way atm
-    public void calculateX(){
-
+        for (int i = 4; i > 0; i--) {
+            if (coefficients[i] != 0.) {
+                potenz = i;
+                i = 0;
+            }
+        }
+       highestPotenz = potenz + ". Grades";
     }
 
-    public void getHighestPotenz(){
+    private void checkForSymmetry() {
+        int totalNumbers = 0;
+        int evenNumbers = 0;
+        int oddNumbers = 0;
 
+        for (int i = 0; i< coefficients.length; i++){
+            if(coefficients[i] != 0){
+                if(i % 2 == 0) evenNumbers ++; else oddNumbers++;
+                totalNumbers++;
+            }
+        }
+
+        if(evenNumbers == totalNumbers){
+            symmetry = SYMMETRIES[0];
+        }
+        if(oddNumbers == totalNumbers){
+            symmetry = SYMMETRIES[1];
+        }
+        symmetry = SYMMETRIES[2];
     }
 
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("f(x) =");
+        double[] temp = coefficients;
+        int firstValueIndex = -1;
+        for (int i = coefficients.length - 1; i >= 0; i--) if (coefficients[i] != 0) firstValueIndex = i;
 
+        for (int i = temp.length - 1; i >= 0; i--) {
+            if (temp[i] != 0) {
+                builder.append(temp[i]);
+                if (i != 0) {
+                    builder.append("x");
+                    if (i != 1) builder.append("^").append(i);
 
-    public void checkPolynomSymmetrical(String[] coefficient){
+                    if (i > firstValueIndex) {
+                        if (i - 1 != 0 && temp[i - 1] < 0) {
+                            builder.append(" - ");
+                            temp[i - 1] *= -1;
+                        } else {
+                            builder.append(" + ");
+                        }
+                    }
+                }
+            }
 
+        }
+        System.out.println(builder);
+        return builder.toString();
     }
-
-
-
-    public void checkPolynomAsymmetrical(String[] coefficient){
-
-    }
-
-
-    public void getFirstDerivation(String[] coefficient){
-
-    }
-
-
-    public void getSecondDerivation(String[] coefficient){
-
-    }
-
 }
