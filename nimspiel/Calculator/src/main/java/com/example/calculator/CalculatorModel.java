9cded23177fd0e49776c19c1e6ff23a3c3267c57
marco411 <vorndamme2003@gmail.com>
2023-01-24 13:16:27 +0100

    Mach etwas zuhause

--- nimspiel/Calculator/src/main/java/com/example/calculator/CalculatorModel.java
+++ nimspiel/Calculator/src/main/java/com/example/calculator/CalculatorModel.java
@@ -2,5 +2,27 @@ package com.example.calculator;
 
 public class CalculatorModel {
 
+    private double operator1;
+    private double operator2;
+    private double result;
 
+    public CalculatorModel(double operator1, double operator2){
+        this.operator1 = operator1;
+        this.operator2 = operator2;
+    }
+    public double addition(double operator1, double operator2) {
+        return result = operator1 + operator2;
+    }
+
+    public double substraction(double operator1, double operator2){
+        return result = operator1 - operator2;
+    }
+
+    public double devition(double operator1, double operator2) {
+        return result = operator1 / operator2;
+    }
+
+    public double multiplication(double operator1, double operator2){
+        return result = operator1 * operator2;
+    }
 }
